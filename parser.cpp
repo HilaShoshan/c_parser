4f1a1dad6c50a131af17fdb6bb38f53956ac068b
Hila <hilashoshan0605@gmail.com>
2021-05-24 15:55:09 +0300

    fixed bugs - done

--- parser.cpp
+++ parser.cpp
@@ -4,7 +4,7 @@ void Parser::parse() {
     terminal a = nextToken();  // the first symbol (nonterminal) of w 
     Symbol X = stack.back();  // the top stack symbol
     while (X.getSymbol() != terminal::END) {  // while stack is not empty
-        // cout << X.getSymbol() << " " << a << endl;
+        // cout << "****** " << X.getType() << " " << X.getSymbol() << " " << a << endl;
         if (X.getType() == Type::TERM && X.getSymbol() == a) {
             printLM();
             stack.pop_back();
@@ -17,10 +17,17 @@ void Parser::parse() {
         else {  // table[X,a] = x -> Y1Y2...Yk
             printLM();
             vector<Symbol> production = rules.at(table.at(X.getSymbol()).at(a));
+            // cout << "number of rule: " << table.at(X.getSymbol()).at(a) << endl;
             stack.pop_back();
             for (Symbol s : production) {
+                if (s.getType() == Type::TERM && s.getSymbol() == terminal::EPSILON)
+                    break;  // do not enter it to the stack 
                 stack.push_back(s);
             }
+        }
+        if (stack.empty()) {
+            printLM();
+            break;
         }
         X = stack.back();  // the top stack symbol
     }
